2815778cc38b1aed4b6f0a610fca4dc2209d94b9
Emmanuel Octavio Martinez Calderon <emmanueloctaviomc@gmail.com>
2022-08-30 20:04:42 -0500

    Base abstract model created

---
+++ app/volando/models.py
@@ -0,0 +1,23 @@
+# Django
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        _('Created at'),
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        _('Updated at'),
+        auto_now=True
+    )
+
+    is_active = models.BooleanField(
+        _('Is active'),
+        default=True
+    )
+
+    class Meta:
+        abstract = True
