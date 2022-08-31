c23813fcfb58f5547c0c536b366ddb1730c47945
Emmanuel Octavio Martinez Calderon <emmanueloctaviomc@gmail.com>
2022-08-30 22:29:39 -0500

    Validate point in warrent serializer

--- app/warrants/serializers.py
+++ app/warrants/serializers.py
@@ -1,3 +1,9 @@
+# Django
+from django.utils.translation import gettext_lazy as _
+
+# GEODJango
+from django.contrib.gis.geos import Point
+
 # Django REST Framework
 from rest_framework import serializers
 
@@ -9,3 +15,15 @@ class WarrantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Warrant
         fields = '__all__'
+
+    def _validate_point(self, value: list) -> Point:
+        try:
+            return Point(value)
+        except TypeError:
+            raise serializers.ValidationError(_('Invalid geopoint'))
+
+    def validate_origin_location(self, value: list) -> Point:
+        return self._validate_point(value)
+
+    def validate_destination_location(self, value: list) -> Point:
+        return self._validate_point(value)
